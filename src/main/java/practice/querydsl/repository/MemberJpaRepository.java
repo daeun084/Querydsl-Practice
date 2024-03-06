fb383502130a9a284d96c0546af4b7e7d50edb40
daeun084 <030804jk@naver.com>
2024-03-06 13:56:36 +0900

    test: 샘플데이터, controller 추가

--- src/main/java/practice/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/practice/querydsl/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package practice.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
@@ -15,6 +17,7 @@ import practice.querydsl.entity.QMember;
 import java.util.List;
 import java.util.Optional;
 
+import static org.springframework.util.StringUtils.hasText;
 import static practice.querydsl.entity.QMember.member;
 import static practice.querydsl.entity.QTeam.team;
 
@@ -63,16 +66,18 @@ public class MemberJpaRepository {
                 .fetch();
     }
 
+    //동적 쿼리 + 조회 성능 최적화
+
     public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition){
 
         BooleanBuilder booleanBuilder = new BooleanBuilder();
 
         //springframework method 사용 -> null check
-        if(StringUtils.hasText(condition.getUsername())) {
+        if(hasText(condition.getUsername())) {
             booleanBuilder.and(member.username.eq(condition.getUsername()));
         }
 
-        if(StringUtils.hasText(condition.getTeamName())){
+        if(hasText(condition.getTeamName())){
             booleanBuilder.and(team.name.eq(condition.getTeamName()));
         }
 
@@ -97,4 +102,40 @@ public class MemberJpaRepository {
                 .where(booleanBuilder) //builder으로 조건 필터링
                 .fetch();
     }
+
+    public List<MemberTeamDto> searchByWhere(MemberSearchCondition condition){
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"), //id mapping
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+    }
+
+        //조건 함수 재사용 가능
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+
 }
